46a7e07a7aab6770fa3eb0b7431665dc90f29ff8
Shark-44 <joanny.bernardeau@gmail.com>
2024-10-21 16:12:23 +0200

    integration de tailwind

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -5,12 +5,17 @@ interface ProductCardProps {
 }
 
 function ProductCard({ product }: ProductCardProps) {
+    const API_URL = import.meta.env.VITE_BACKEND_URL
     return (
-        <div className="product-card">
-            <h2>{product.nomproduit}</h2>
-            <img src={product.photoProduit} alt={product.nomproduit} />
-            <p>{product.descriptionProduit}</p>
-        </div>
+        <div className="bg-white shadow-md rounded-lg overflow-hidden p-4 m-2">
+        <h2 className="text-xl font-semibold mb-2">{product.nomproduit}</h2>
+        <img
+            src={API_URL + product.photoProduit}
+            alt={product.nomproduit}
+            className="w-full h-48 object-cover mb-2"
+        />
+        <p className="text-gray-700">{product.descriptionProduit}</p>
+    </div>
     );
 }
   
