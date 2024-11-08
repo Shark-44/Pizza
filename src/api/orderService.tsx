0a52b51c161d46caac660a694a9d7d783a824f83
Shark-44 <joanny.bernardeau@gmail.com>
2024-11-08 10:30:28 +0100

    wrapper ok gestion des messages d'error a voir

--- src/api/orderService.tsx
+++ src/api/orderService.tsx
@@ -23,14 +23,14 @@ export const createOrder = async (
 
   return apiCall<Order>('post', '/orders', {
     data: orderData,
-    errorNamespace: 'api.create.orders',
+    errorNamespace: '{lang}.api.create.orders',
   });
 };
 
 // Supprimer une commande
 export const deleteOrder = async (id: number): Promise<void> => {
   return apiCall<void>('delete', `/orders/${id}`, {
-    errorNamespace: 'api.delete.orders',
+    errorNamespace: '{lang}.api.delete.orders',
   });
 };
 
@@ -38,7 +38,7 @@ export const deleteOrder = async (id: number): Promise<void> => {
 export const finalOrder = async (id: number, lang: string): Promise<FullOrder> => {
   return apiCall<FullOrder>('get', `/orders/${id}?lang=${lang}`, {
     params: { lang },
-    errorNamespace: 'api.fetch.orders',
+    errorNamespace: '{lang}.api.fetch.finalorders',
   });
 };
 
@@ -52,18 +52,6 @@ export const finishOrder = async (orderId: number, prixtotalCommande: number): P
 
   return apiCall<Order>('put', `/orders/${orderId}`, {
     data: orderData,
-    errorNamespace: 'api.finish.orders',
+    errorNamespace: '{lang}.api.finish.orders',
   });
 };
-
-
-// Sauvegarde perso
-
-  //console.error('Erreur lors de la supression de la commande:', error);
-  //throw new Error('Impossible de de supprimer une commande');
-
-  //console.error(`Erreur lors de la récupération des commandes (ID: ${id}):`, error);
-  //throw new Error('Impossible de récupérer les commandes'
-
-  //console.error('Erreur lors de la mise à jour de la commande:', error);
-  //throw new Error('Impossible de mettre à jour la commande');
