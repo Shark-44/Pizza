4c6ce25aa9b1c6482c811d6e53fd72bc5c4a12d6
Shark-44 <joanny.bernardeau@gmail.com>
2024-11-09 13:05:15 +0100

    creation d'un popup pour avertir qu'un utilisateur a été créé

--- src/pages/CreateUser.tsx
+++ src/pages/CreateUser.tsx
@@ -1,22 +1,28 @@
 import { useState } from "react";
 import { createUser } from "../api/userService";
+import PopUp from '../components/specificPageComponents/PopUp';
 
 const CreateUser = () => {
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
- 
-  const [error, setError] = useState<string | null>(null);  
+  const [error, setError] = useState<string | null>(null);
+  const [openPopup, setOpenPopup] = useState(false);
 
   const handleSubmit = async () => {
     try {
       const loggedInUser = await createUser(name, password);
-      console.log("Utilisateur créé:", loggedInUser); 
-     
+      console.log("Utilisateur créé:", loggedInUser);
+      setOpenPopup(true);
       setError(null);
     } catch (err) {
-      console.error("Erreur de connexion :", err); 
+      console.error("Erreur de connexion :", err);
       setError("Erreur de connexion.");
+      setOpenPopup(false);
     }
+  };
+
+  const closePopUp = () => {
+    setOpenPopup(false);
   };
 
   return (
@@ -24,7 +30,7 @@ const CreateUser = () => {
       <div className="bg-white p-8 rounded-lg shadow-lg w-96">
         <form onSubmit={(e) => e.preventDefault()}>
           <h1 className="text-3xl font-bold text-center text-gray-800 mb-6">
-            Creer un identifiant
+            Créer un identifiant
           </h1>
 
           <label htmlFor="login-input" className="block text-lg text-gray-700 mb-2">
@@ -56,7 +62,7 @@ const CreateUser = () => {
             }}
           />
 
-          {error && <p className="text-red-500 mb-4">{error}</p>} 
+          {error && <p className="text-red-500 mb-4">{error}</p>}
 
           <input
             type="button"
@@ -65,6 +71,8 @@ const CreateUser = () => {
             className="w-full bg-blue-500 text-white py-3 rounded-lg hover:bg-blue-600 focus:outline-none"
           />
         </form>
+
+        <PopUp openPopUp={openPopup} closePopUp={closePopUp} />
       </div>
     </div>
   );
